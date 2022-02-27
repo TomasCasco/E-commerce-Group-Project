ca0d852c4ed9cb17c9997c9c3f6ba5f07dd36c5f
joel-CM <cjoel1977@gmail.com>
2022-02-27 13:14:24 -0300

    .env added to gitignore

--- api/src/database.js
+++ api/src/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.URL_MONGO)
-  .then(async () => {
+  .then(() => {
     console.log("db connected");
   })
   .catch((err) => {

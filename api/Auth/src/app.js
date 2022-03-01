c3b23e30c3f7b51efaed588df0c0dd68facde422
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-01 17:49:50 -0300

    create api on src folder

---
+++ api/Auth/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const server = express();
+
+server.serverName= "AUTH"
+
+server.set("port", process.env.PORT || 3002);
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+
+
+module.exports = server;

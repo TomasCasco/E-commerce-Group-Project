6fb8c8e68047be41e5ff42e69bf48f715c503895
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-01 17:50:21 -0300

    create model user

---
+++ api/Auth/src/models/User.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+
+//exporto una funcion que define el modelo,
+//luego le injectamos la conexion sequelize
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: "Username in use",
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: "Email in use",
+      },
+      validate: {
+        isEmail: {
+          msg: "Invalid email type",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};

d78b630fbf584b2f9db2ad76756b87e27f8b71de
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-02 21:15:34 -0300

    change register status code returns

--- api/Auth/src/controllers/register.js
+++ api/Auth/src/controllers/register.js
@@ -1,25 +1,21 @@
-const { BCRYPT_ROUNDS } = process.env;
-const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 const register = async (req, res, next) => {
   let { username, email, password } = req.body;
-  if (password.length < 6 || password.length > 20) {
-    throw new Error("The password must be between 6 and 20 characters");
-  }
-  //encriptamos password
-  password = bcrypt.hashSync(password, parseInt(BCRYPT_ROUNDS));
-  //creamos usuario
-  const newUser = new User({
-    username,
-    email,
-    password,
-  });
-
   try {
+    if (password.length < 6 || password.length > 20) {
+      throw new Error("The password must be between 6 and 20 characters");
+    }
+    //creamos usuario
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
     const savedUser = await newUser.save();
     res
-      .status(200)
+      .status(201)
       .json({ msg: `User created successfully`, userId: savedUser._id });
   } catch (error) {
     next(error);

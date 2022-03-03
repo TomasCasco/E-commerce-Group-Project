d176d7cfc916eb69be58678125fbb47fcefaaa02
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-03 14:43:55 -0300

    add access route

--- api/Auth/src/middlewares/auth.js
+++ api/Auth/src/middlewares/auth.js
@@ -4,12 +4,14 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   let token = req.headers.authorization.split(" ")[1];
+  if (!token) return res.status(401).json({error:"Invalid credentials"})
   jwt.verify(token, JWT_SECRET, (err, verifiedJwt) => {
     if (err) {
-      next(err);
+     return next(err);
     } else {
       //respondo true si la firma del token es correcta
-      res.status(200).json(true);
+      req.user=verifiedJwt.userId;
+     return next();
     }
   });
 };
--- api/Auth/src/routes/userRouter.js
+++ api/Auth/src/routes/userRouter.js
@@ -5,12 +5,14 @@ const auth = require("../controllers/auth");
 const getUserInfo = require("../controllers/getUserInfo");
 const getCart = require("../controllers/getCart");
 const editCart = require("../controllers/editCart");
+const access = require("../controllers/access");
+
 
 const router = Router();
 
 router.post("/login", login);
 router.post("/register", register);
-router.post("/verifyToken", auth);
+router.post("/access",auth,access)
 router.get("/:userId/", getUserInfo);
 router.get("/:userId/cart", getCart);
 router.post("/:userId/cart", editCart);

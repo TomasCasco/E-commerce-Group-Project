e0360e535e277db90c5d1590c39f8c2eb31af7c1
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-03 20:33:14 -0300

    add route create payment with mercadopago

--- api/Bills/src/app.js
+++ api/Bills/src/app.js
@@ -4,8 +4,10 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const setHeaders = require("./middlewares/setHeaders");
 const errorHandler = require("./middlewares/errorHandler");
+const router=require("./routes")
 
 require("./config/db");
+
 
 const server = express();
 
@@ -13,12 +15,13 @@ server.serverName = "BILLS";
 
 server.set("port", process.env.PORT || 3003);
 
-
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(setHeaders);
+
+server.use("/bills",router)
 
 server.use(errorHandler);
 

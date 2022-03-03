07483b92f86090465330489d8d2f9eccfb1bb260
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-03 15:39:58 -0300

    change res access controller

--- api/Auth/src/routes/userRouter.js
+++ api/Auth/src/routes/userRouter.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const login = require("../controllers/login");
 const register = require("../controllers/register");
-const auth = require("../controllers/auth");
 const getUserInfo = require("../controllers/getUserInfo");
 const getCart = require("../controllers/getCart");
 const editCart = require("../controllers/editCart");
 const access = require("../controllers/access");
+const auth = require("../middlewares/auth");
 
 
 const router = Router();
@@ -15,6 +15,6 @@ router.post("/register", register);
 router.post("/access",auth,access)
 router.get("/:userId/", getUserInfo);
 router.get("/:userId/cart", getCart);
-router.post("/:userId/cart", editCart);
+router.put("/:userId/cart", editCart);
 
 module.exports = router;

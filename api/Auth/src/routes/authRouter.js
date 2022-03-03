3475a88f1e8123a8d215be8d7af2349e6ee0e3e5
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-02 21:17:48 -0300

    add routes on router

--- api/Auth/src/routes/authRouter.js
+++ api/Auth/src/routes/authRouter.js
@@ -2,12 +2,17 @@ const { Router } = require("express");
 const login = require("../controllers/login");
 const register = require("../controllers/register");
 const auth = require("../controllers/auth");
+const getUserInfo = require("../controllers/getUserInfo");
+const getCart = require("../controllers/getCart");
+const editCart = require("../controllers/editCart");
 
 const router = Router();
 
 router.post("/login", login);
 router.post("/register", register);
 router.post("/verifyToken", auth);
-
+router.get("/:userId/", getUserInfo);
+router.get("/:userId/cart", getCart);
+router.post("/:userId/cart", editCart);
 
 module.exports = router;

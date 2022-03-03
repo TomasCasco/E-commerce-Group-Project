d077a9a8361e4a88356faa16e33c791b2a122d46
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-02 21:14:51 -0300

    controller getCart

---
+++ api/Auth/src/controllers/getCart.js
@@ -0,0 +1,20 @@
+const User = require("../models/User");
+const Cart = require("../models/Cart");
+
+const getCart = async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    if (userId && (await User.findById(userId))) {
+      let cart = await Cart.findOne({ userId });
+      !cart ? (cart = await Cart.create({ userId })) : null;
+      const {products}= cart
+      res.status(200).json({userId,products});
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getCart;

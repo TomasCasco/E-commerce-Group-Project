26f31ed2e4cfb0ea1dafe3101fc91333bcf2eecf
gon salinas <gonzalosalinasss.96@gmail.com>
2022-03-02 21:19:27 -0300

    --

--- api/Auth/src/routes/index.js
+++ api/Auth/src/routes/index.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
 
-const authRouter = require("./authRouter");
+const userRouter = require("./userRouter");
 
 const router = Router();
 
-router.use("/users", authRouter);
+router.use("/users", userRouter);
 
 
 module.exports = router;

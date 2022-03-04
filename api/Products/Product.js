1c1ab96d3491126a2e378ff3ff4d4d4d3267a6e3
Joel Chavez <84867719+joel-CM@users.noreply.github.com>
2022-03-04 14:52:54 -0300

    Joel 2 (#22)
    * se agrego propiedad stock y un readme.txt sobre las rutas
    
    * se agrego busqueda por type y ordenamientos
    
    * se agregaron todas las querys busqueda y filtrado
    
    Co-authored-by: joel-CM <cjoel1977@gmail.com>

--- api/Products/Product.js
+++ api/Products/Product.js
@@ -6,8 +6,8 @@ const Product = new Schema({
   brand: { type: String, required: true },
   image: { type: String, required: true },
   description: { type: String, required: true },
-  stock: { type: Number, default: 100, required: true },
-  type: { type: String, required: true },
+  stock: { type: Number, default: 0, required: true },
+  category: { type: String, required: true },
 });
 
 module.exports = model("Product", Product);

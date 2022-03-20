f66056f8abeb3d0d1dfc5f5745df9cddd1a79ba7
Ignacio Lopez <92762013+NachooLopez@users.noreply.github.com>
2022-03-20 15:12:01 -0300

    Nacho/checkout (#64)
    * webhook
    
    * webhook 2
    
    * feature: bills

--- api/ApiGateway/src/resolvers/index.js
+++ api/ApiGateway/src/resolvers/index.js
@@ -1,7 +1,12 @@
 const { mergeResolvers } = require("@graphql-tools/merge");
 const userResolvers = require("./auth");
 const productResolvers = require("./products");
+const billResolvers = require("./bills");
 
-const resolvers = mergeResolvers([userResolvers, productResolvers]);
+const resolvers = mergeResolvers([
+  userResolvers,
+  productResolvers,
+  billResolvers,
+]);
 
 module.exports = resolvers;
---
+++ client/redux/checkout/checkoutActions.js
@@ -0,0 +1,37 @@
+import { client } from "../../apolloClient/apolloClient";
+import { queryBills, queryBuyFromCheckout } from "../../apolloClient/querys";
+
+export const buyFromCheckout =
+  ({ cart, userId, email }) =>
+  async () => {
+    const cartCheckout = cart.map((product) => {
+      return {
+        id: product.product._id,
+        title: product.product.name,
+        unit_price: product.product.price,
+        picture_url: product.product.image,
+        quantity: product.qty,
+      };
+    });
+
+    try {
+      const response = await client.query({
+        query: queryBuyFromCheckout,
+        variables: {
+          cart: cartCheckout,
+          userId,
+          email,
+        },
+      });
+      return response;
+    } catch (e) {
+      console.log(JSON.stringify(e, null, 2));
+    }
+  };
+
+export const getBills = (userId) => {
+  return {
+    type: "GET_BILLS",
+    payload: userId,
+  };
+};

52422b7c965515799fc5b680408a134641ffb6ce
Tomás Casco <85589457+TomasCasco@users.noreply.github.com>
2022-03-21 14:08:15 -0300

    UI changes (#77)
    * cart
    
    * Faqs
    
    * footer
    
    * enter
    
    * getById
    
    * deploy
    
    * fixes
    
    * detail
    
    * some changes
    
    * some changes
    
    * ui changes
    
    * Update index.js
    
    Co-authored-by: Ignacio Lopez <92762013+NachooLopez@users.noreply.github.com>

--- client/pages/checkout.js
+++ client/pages/checkout.js
@@ -1,11 +1,13 @@
 import { Box, Link } from "@chakra-ui/react";
 import NavBar from "../components/Navbar/NavBar";
 import Checkout from "../components/Cart/Checkout";
+import Nav from "../components/Navbar/NavResponsive";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Nav />
       <Checkout />
     </>
   );

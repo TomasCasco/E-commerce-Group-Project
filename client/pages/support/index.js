a12aced9285e08691cbf69da08ba94fc98788593
Tomás Casco <85589457+TomasCasco@users.noreply.github.com>
2022-03-23 10:38:39 -0300

    Update index.js

--- client/pages/support/index.js
+++ client/pages/support/index.js
@@ -3,7 +3,7 @@ import Footer from "../../components/Footer/Footer.tsx";
 
 import NavBar from "../../components/Navbar/NavBar";
 import Nav from "../../components/Navbar/NavResponsive";
-import ContactUsForm from "../../components/Support/Contactus";
+
 import { Faqs } from "../../components/Support/Faqs";
 import { Terms } from "../../components/Support/Terms";
 
@@ -27,7 +27,7 @@ export default function Support() {
         currentTermId={currentTermId}
         setCurrentTermId={setCurrentTermId}
       />
-      {/* <ContactUsForm /> */}
+      
       <Footer />
     </>
   );
